8461197d15b156878a69a09648b26ef3d6e7fdbc
qiwu-zhang <qiwu.zhang@mail.utoronto.ca>
2020-08-21 08:27:38 -0400

    updated request

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template
 from flask import request
 import database_utilities as dbHandler
@@ -8,9 +10,18 @@ app = Flask(__name__)
 @app.route('/', methods=['POST', 'GET'])
 def hello_world():
     if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        os.chdir(os.path.dirname(__file__))
+        path = os.path.join(os.getcwd(),"LoginInfo.db")
+        dbHandler.create_database(database_path=path)
+        dbHandler.insert_User(database_path=path, userName=email, Password=password)
         return render_template('signlog.html')
+
     else:
-        return render_template('signlog.html')
+       return render_template('signlog.html')
+
 
 
 if __name__ == '__main__':
--- database_utilities.py
+++ database_utilities.py
@@ -6,30 +6,12 @@ def create_database(database_path: str):
     with conn:
         cur = conn.cursor()
         cur.execute("drop table if exists userInfo")
-        ddl = "create table loginInfo(userName string not null constraint loginInfo_pk primary key,Password string not null);"
-        cur.execute(ddl)
-        ddl = "create unique index loginInfo_User_Name_uindex on loginInfo(userName);"
+        ddl = "create table userInfo(userName str not null,Password str not null);"
         cur.execute(ddl)
     conn.close()
 
 
-def signUp(database_path: str, userName: str, Password: str):
+def insert_User(database_path: str, userName: str, Password: str):
     conn = lite.connect(database_path)
     cur = conn.cursor()
-    cur.execute("INSERT INTO loginInfo(userName, Password) VALUES (?,?)", (userName, Password))
-
-
-def LogIn(database_path: str, userName: str, Password: str):
-    conn = lite.connect(database_path)
-    with conn:
-        cur = conn.cursor()
-        # check to see if the user is in there
-        sql = "select count(userName) from loginInfo where userName='" + userName + "'"
-        cur.execute(sql)
-        count = cur.fetchone()[0]
-        if count > 0:
-            pass
-        else:
-            print("User doesn't exist!")
-
-
+    cur.execute("INSERT INTO userInfo(userName, Password) VALUES (?,?)", (userName, Password))
