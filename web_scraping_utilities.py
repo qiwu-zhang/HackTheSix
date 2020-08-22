2af0912a79396caf51284a4f70b720af83717349
qiwu-zhang <62823758+qiwu-zhang@users.noreply.github.com>
2020-08-22 11:18:22 -0400

    Update web_scraping_utilities.py

--- web_scraping_utilities.py
+++ web_scraping_utilities.py
@@ -1,5 +1,6 @@
 import re
 import string
+import requests
 from urllib.request import urlopen
 
 from bs4 import BeautifulSoup
@@ -15,51 +16,15 @@ def load_urls_from_file(file_path: str):
         exit(2)
 
 
-def load_page(url: str):
-    response = urlopen(url)
-    html = response.read().decode('utf-8')
-    return html
 
+def scrape_page(url: str):
+    response = request.get(url)
+    soup = BeautifulSoup(response.text, "lxml")
+    name = soup.find_all('span', class_= "provider-name ng-binding")
+    type = soup.find_all('h1', class_='ng-scope')
+    rate = soup.find_all('span', class_= 'ng-binding')
+    for i in range(0,len(name)):
+        print(type)
+        print(name[i].text)
+        print(rate[i].text)
 
-def scrape_page(page_contents: str):
-    chicken_noodle = BeautifulSoup(page_contents, "html5lib")
-
-    for script in chicken_noodle(["script", "style"]):
-        script.extract()
-
-    text = chicken_noodle.get_text()
-    lines = (line.strip() for line in text.splitlines())
-    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-
-    text = ' '.join(chunk for chunk in chunks if chunk)
-    plain_text = ''.join(filter(lambda x: x in string.printable, text))
-
-    clean_words = []
-
-    words = plain_text.split(" ")
-    for word in words:
-        clean = True
-
-        # no punctuation
-        for punctuation_marks in string.punctuation:
-            if punctuation_marks in word:
-                clean = False
-
-                # no numbers
-            if any(char.isdigit() for char in word):
-                clean = False
-
-                # at least two characters but no more than 10
-            if len(word) < 2 or len(word) > 10:
-                clean = False
-
-            if not re.match(r'^\w+$', word):
-                clean = False
-
-            if clean:
-                try:
-                    clean_words.append(word.lower())
-                except UnicodeEncodeError:
-                    print(".")
-
-    return clean_words
