e65fce831ee299f170f2b2ccf5659b105f4d1377
harris222 <harris222@users.noreply.github.com>
2020-08-22 01:32:19 -0700

    Optimized code in database utilities and apps

--- database_utilities.py
+++ database_utilities.py
@@ -1,69 +1,59 @@
+import os
 import sqlite3 as lite
+from sqlite3 import IntegrityError
 
 
-def create_database(database_path: str):
-    conn = lite.connect(database_path)
-    with conn:
-        cur = conn.cursor()
-        cur.execute("drop table if exists userInfo")
-        ddl = "create table userInfo(userName str not null,Password str not null,age int default 18 not null,city text default Toronto not null,monthly_income int default 4000 not null, yearly_income int default 10000 not null,saving_goaly_yearly int default 5000,saving_plan_1 text default unknown not null,saving_plan_2 text default unknown not null,saving_plan_3 text default unknown not null);"
-        cur.execute(ddl)
-    conn.close()
-
-
-def insert_User(database_path: str, userName: str, Password: str):
-    conn = lite.connect(database_path)
+def make_connection():
+    global conn
+    global cur
+    os.chdir(os.path.dirname(__file__))
+    path = os.path.join(os.getcwd(), "LoginInfo.db")
+    conn = lite.connect(path)
     cur = conn.cursor()
-    cur.execute("insert into userInfo(userName, Password) values (?,?)", (userName, Password))
+    print(conn)
+    print(cur)
+
+def create_table():
+    cur.execute("drop table if exists userInfo")
+    ddl = "create table userInfo(userName str not null,Password str not null,age int default 18 not null,city text default Toronto not null,monthly_income int default 4000 not null, yearly_income int default 10000 not null,saving_goaly_yearly int default 5000,saving_plan_1 text default unknown not null,saving_plan_2 text default unknown not null,saving_plan_3 text default unknown not null);"
+    cur.execute(ddl)
+
+
+def insert_User(userName: str, Password: str):
+    try: 
+        cur.execute("insert into userInfo(userName, Password) values (?,?)", (userName, Password))
+    except IntegrityError as e:
+        print("Duplicate user")   
+    
     conn.commit()
-    conn.close()
 
-
-def login(database_path: str, userName: str, Password: str):
-    print("%s" % userName)
-    print("%s" % Password)
-    conn = lite.connect(database_path)
-    cur = conn.cursor()
+def login(userName: str, Password: str):
+    # print("%s" % userName)
+    # print("%s" % Password)
     try:
-        cur.execute("select userName from userInfo")
-        userName_collection = cur.fetchall()
-        print("in the userName_collection we have ")
-        print("%s" % userName_collection)
-
-        for row in userName_collection:
-            if userName in row:
-                print("%s" % userName)
-                cur.execute("select Password from userInfo WHERE userName = ?", (userName,))
-                pw = cur.fetchall()
-                print("%s" % pw)
-
-                if Password in pw:
-                    print("login successful")
-                    conn.commit()
-                    conn.close()
-                    return 1
-                else:
-                    print("password isn't correct")
-
-            else:
-                print("Username does not exist")
-
+        cur.execute("select userName, Password from userInfo where userName = ?", (userName,)) ## Select only username that you care about
+        results = cur.fetchall()
+        if len(results) == 0: 
+            return 0
+    
+        if (userName, Password) == (results[0][0], str(results[0][1])): 
+            print ("login successful")
+            return 1
     except IOError as e:
         print(e)
-    conn.commit()
-    conn.close()
+
     return 0
 
 
-def update_profile(database_path: str, userName: str, age: int, city: str, monthly_income:int, yearly_income:int,saving_goal:int):
-    conn = lite.connect(database_path)
-    cur = conn.cursor()
+def update_profile( userName: str, age: int, city: str, monthly_income:int, yearly_income:int,saving_goal:int):
     cur.execute('''UPDATE userInfo SET age = ? WHERE userName = ?''', (age, userName))
     cur.execute('''UPDATE userInfo SET city = ? WHERE userName = ?''', (city, userName))
     cur.execute('''UPDATE userInfo SET monthly_income = ? WHERE userName = ?''', (monthly_income, userName))
     cur.execute('''UPDATE userInfo SET yearly_income = ? WHERE userName = ?''', (yearly_income, userName))
     cur.execute('''UPDATE userInfo SET saving_goal = ? WHERE userName = ?''', (saving_goal, userName))
     conn.commit()
+
+def close():
     conn.close()
 
 
