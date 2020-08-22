22c7d675344ab3f3aa656ddd555361d088314b6b
Emily Bao <emily.yf.bao@gmail.com>
2020-08-21 23:30:50 -0600

    Added skeleton for input form

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import os
 
-from flask import Flask, render_template
-from flask import request
+from flask import Flask, render_template, flash
+from flask import request, redirect
+from wtforms import Form, TextField 
 import database_utilities as dbHandler
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -27,8 +30,18 @@ def hello_world():
         
 
 
+@app.route('/inputForm', methods = ["GET", "POST"])
+def input_form():
+    return render_template("inputForm.html")
+
+@app.route('/done')
+def place_holder():
+    return render_template("tables.html")
+
 if __name__ == '__main__':
     os.chdir(os.path.dirname(__file__))
     path = os.path.join(os.getcwd(), "LoginInfo.db")
     dbHandler.create_database(database_path=path)
     app.run()
+
+
