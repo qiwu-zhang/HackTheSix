2a060bbe34bd099ad3c4a54bc9f658693fed1d3d
qiwu-zhang <62823758+qiwu-zhang@users.noreply.github.com>
2020-08-22 05:16:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import os
-
+import argparse
+from bs4 import BeautifulSoup
 from flask import Flask, render_template, redirect
 from flask import request
+<<<<<<< HEAD
+import web_scraping_utilities
+=======
 from enum import IntEnum
+>>>>>>> e65fce831ee299f170f2b2ccf5659b105f4d1377
 import database_utilities as dbHandler
 
 app = Flask(__name__)
@@ -38,6 +43,17 @@ def first_step():
 def dashboard():
     return render_template('index.html')
 
+
+def main(database: str, url_list_file: str):
+    saving_plan_list = []
+    print("we are going to work with " + database)
+    print("we are going to scan " + url_list_file)
+    urls = web_scraping_utilities.load_urls_from_file(url_list_file)
+    for url in urls:
+        print("reading " + url)
+        page_content = web_scraping_utilities.load_page(url=url)
+        saving_plans = web_scraping_utilities.scrape_page(page_content)
+        saving_plan_list.extend(saving_plans)
 
 
 if __name__ == '__main__':
